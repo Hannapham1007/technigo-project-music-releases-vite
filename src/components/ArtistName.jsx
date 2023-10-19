ca68390aa3aebe771e394dca105bd6018d922ed9
Hannapham1007 <hannaphamm1007@gmail.com>
2023-10-20 00:43:24 +0200

    add album name, artist name

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./ArtistName.css";
 
-export const ArtistName = (props) => {
-    // TODO: Link artist name, Hanna
-    return <div className='artist-name'>
-    Artist: {props.name}
-</div>
-}
+export const ArtistName = ({ artistsArray }) => {
+  // TODO: Link artist name, Hanna
+  return (
+    <div className="artist-name">
+      {artistsArray.map((artist, index) => {
+        return (
+          <a key={artist.id} href={artist.external_urls.spotify}>
+           <p className="single-artist">{artist.name}</p> 
+           {/*if the current artist is not the last one in the array, then adds the comma */}
+           {index < artistsArray.length - 1 && <span>, </span>}
+          </a>
+        );
+      })}
+    </div>
+  );
+};
