9024f1afa7464e38d2c9efa2fa35a0f9834fdbe7
karo <karolina.shi@outlook.com>
2023-10-20 19:21:00 +0200

    Update Header-component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import "../styles/header.css";
 
 export const Header = (props) => {
     // To do
-    return <div>
-        Title of website
+    return <div id="header">
+        <h1>New Albums & Singles</h1>
+        <h2>Technigo Education Team</h2>
     </div>
 }
