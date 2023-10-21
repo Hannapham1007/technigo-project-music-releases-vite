d357142ab2f05f1da4e93ff0f9f6561d6befb233
karo <karolina.shi@outlook.com>
2023-10-21 12:31:04 +0200

    Add icons

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/CoverImage.css";
-
+import dotsIcon from "../assets/icons/dots.svg";
+import heartIcon from "../assets/icons/heart.svg";
+import playIcon from "../assets/icons/play.svg";
 
 export const CoverImage = ({coverImage}) => {
+    const [isHidden, setIsHidden] = useState(true)
 
-return (
-    <div className="cover-image">
-        <img src={coverImage.images[1].url}></img> 
-    </div>
-  );
- }
+    const styleIsHidden = {visibility: isHidden ? 'hidden' : 'visible'}
+
+    return (
+        <div className="cover-image" onMouseEnter={() => setIsHidden(false)} onMouseLeave={() => setIsHidden(true)}>
+            <span className="icons-container" style={styleIsHidden}>
+                <img src={heartIcon} alt="heart icon" className="icon"/>
+                <img src={playIcon} alt="play icon" className="icon"/>
+                <img src={dotsIcon} alt="dots icon" className="icon"/>
+            </span>
+            <img src={coverImage.images[1].url} />
+        </div>
+    );
+}
 
 
   
