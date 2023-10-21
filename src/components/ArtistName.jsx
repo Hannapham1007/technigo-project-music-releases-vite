b10ebfcbb83137f87ea1b372cedf70630b616761
karo <karolina.shi@outlook.com>
2023-10-21 10:55:15 +0200

    Add comma and ampersand separation between artist names

--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -6,13 +6,24 @@ export const ArtistName = ({ artistsArray }) => {
   return (
     <div className="artist-name">
       {artistsArray.map((artist, index) => {
-        return (
-          <a key={artist.id} href={artist.external_urls.spotify}>
-           <p className="single-artist">{artist.name}</p> 
-           {/*if the current artist is not the last one in the array, then adds the comma */}
-           {index < artistsArray.length - 1 && <span>, </span>}
-          </a>
-        );
+        if(artistsArray.length == 1) { // If only one artist, display as usual
+          return(
+            <a key={artist.id} href={artist.external_urls.spotify}>
+              <p className="single-artist">{artist.name}</p> 
+            </a>
+          );
+        }else { // Else if several artists, add logic for separation text
+          let lastArtist = artistsArray[artistsArray.length-1];
+          return (
+            <a key={artist.id} href={artist.external_urls.spotify}>
+              {/* if the current artist is last one, add ampersand before */}
+              <span>{ (artist.id == lastArtist.id ? '& ' : ' ') }</span>
+              <p className="single-artist">{artist.name}</p> 
+              {/* if the current artist is not next-to-last one in the array, add comma */}
+              <span>{ (index < artistsArray.length - 2 ? ', ' : ' ') }</span>
+            </a>
+          );
+        }
       })}
     </div>
   );
